a6f88b06a7413c47fb78c319dfddc07330c486a5
Rafael-co <rafael.camilo21@outlook.com>
2023-06-22 14:47:01 -0300

    edição mobile

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 const burger = document.getElementById('navButton');
 const menu = document.getElementById('navBarInicio__elementos');
+const icone = document.getElementById('icone__burger');
 
 burger.addEventListener('click', ()=> {
     const style = window.getComputedStyle(menu);
     const display = style.getPropertyValue('display')
     if(display == "none"){
         menu.style.display = "flex"
+        icone.innerHTML = "close"
     }
     if(display == "flex"){
         menu.style.display = "none"
+        icone.innerHTML = "menu"
     }
 
 });
