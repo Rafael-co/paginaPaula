616502cf905a4523db2ce5efc48a520f6aca5d09
Rafael-co <rafael.camilo21@outlook.com>
2023-07-10 15:45:24 -0300

    beta IV

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const burger = document.getElementById('navButton');
 const menu = document.getElementById('navBarInicio__elementos');
 const icone = document.getElementById('icone__burger');
-
+const accordions = document.querySelectorAll('.accordion');
 burger.addEventListener('click', ()=> {
     const style = window.getComputedStyle(menu);
     const display = style.getPropertyValue('display')
@@ -14,4 +14,24 @@ burger.addEventListener('click', ()=> {
         icone.innerHTML = "menu"
     }
 
+})
+
+
+accordions.forEach((accordion) => {
+  const button = accordion.querySelector('.buttom__accordion');
+  const collapse = accordion.querySelector('.collapse');
+
+  button.addEventListener('click', () => {
+    // Fecha todos os accordions abertos
+    accordions.forEach((item) => {
+      const currentCollapse = item.querySelector('.collapse');
+      console.log(item)
+      if (currentCollapse !== collapse && currentCollapse !=null) {
+        currentCollapse.classList.remove('show');
+      }
+    });
+
+    // Abre ou fecha o accordion clicado
+    collapse.classList.toggle('show');
+  });
 });
